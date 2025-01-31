d61f9a95b8d9f01e862bc7799065aa9e083c54cc
KONNI VIDYA SAGAR <10vidyasagarkonni@gmail.com>
2025-02-01 03:03:29 +0530

    Update test1.py
    Added new output file

--- test1.py
+++ test1.py
@@ -1,3 +1,8 @@
 
 with open("rough.txt", 'w') as file:
     file.write("Hello Sagar, how are you, I'm doing good")
+
+
+
+with open("rough1.txt", 'w') as file:
+    file.write("Hello Sagar, how are you, I'm doing good")
